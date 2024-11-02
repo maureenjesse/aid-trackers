617adae648d6be0570f306502a68df3f7945a082
DavidManiIbrahim <ibrahimmanidavid@gmail.com>
2024-11-02 14:37:03 +0100

    done and dusted

--- src/app/components/Support.js
+++ src/app/components/Support.js
@@ -6,7 +6,7 @@ const Support = () => {
   const title = 'Help the future leaders of Tomorrow';
   const description = 'Born out of a vision, a single-minded objective that puts service before anything else,Swift Clearance and Forwarding Corp. surging forth to deliver the best services in the shopping and logistics scenario. Its meteroric rise stems out of a solid foundation. The management boasts of over 20 years of rich and varied experience in the shipping and freight forwarding industry';
   return (
-    <div>
+    <div className={styles.container}>
       <div className={styles.card}>
       <div className={styles.imageContainer}>
         <Image src='/images/Frame160.png' alt='' className={styles.image} width={400} height={300}/>
