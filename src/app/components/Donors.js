7577a865d0e145042ac1bb96de4287044267230e
DavidManiIbrahim <ibrahimmanidavid@gmail.com>
2024-11-02 22:09:47 +0100

    fixed image bug

--- src/app/components/Donors.js
+++ src/app/components/Donors.js
@@ -6,32 +6,10 @@ export default function Donors() {
         <div className={styles.container} id="donors">
             <h2 className={styles.title}>Donors</h2>
             <div className={styles.images}>
-                <Image
-                    src="/images/image (1).png"
-                    alt="Child receiving aid"
-                    className={styles.heroImage}
-                    width={150}
-                    height={300}
-                />
-
-                <Image
-                    src="/images/image.png"
-                    alt="Center image"
-                    width={400}
-                    height={200}
-                    layout="responsive"
-                    className={styles.heroImage2}
-                />
-
-                <Image
-                    src="/images/image (11).png"
-                    alt="Right image"
-                    width={100}
-                    height={50}
-                    layout="responsive"
-                    className={styles.heroImage3}
-                />
-            </div>
+            <Image src="/images/image (1).png" alt="Child receiving aid" className={styles.heroImage} width={100} height={200}/>
+            <Image src="/images/image.png" alt="Mother with child" className={styles.heroImage2} width={600} height={250}/>
+            <Image src="/images/image (11).png" alt="Child looking at aid worker" className={styles.heroImage3} width={100} height={200}/>
+                           </div>
             <div className={styles.content}>
                 <h3 className={styles.heading}>Register as a Donor</h3>
                 <p className={styles.description}>
