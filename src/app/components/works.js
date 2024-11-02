032df74a28850072d8712efef15193b654e23176
DavidManiIbrahim <ibrahimmanidavid@gmail.com>
2024-11-02 21:20:59 +0100

    style link bug fixed

--- src/app/components/works.js
+++ src/app/components/works.js
@@ -1,6 +1,6 @@
 // components/HowItWorks.js
 import Image from 'next/image';
-import styles from '..//styles/Works.module.css';
+import styles from '../styles/Works.module.css';
 
 export default function Works() {
   const steps = [
