e7c3c8cc48718436d2d4ecb4175eba3d88b582fc
DavidManiIbrahim <ibrahimmanidavid@gmail.com>
2024-11-03 07:19:25 +0100

    fixed logo bug

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -7,8 +7,10 @@ import styles from "../styles/Header.module.css";
 const Header = () => {
   return (
     <header className={styles.header}>
-         <Image src="/images/Union.png" alt="company logo" className={styles.companylogo} width={20} height={20}/>
-      <div className={styles.logo}> <span className={styles.highlight}>Aid</span>  Tracker</div>
+          <div className={styles.logocontainer}>
+      <Image src="/images/Union.png" alt="company logo" className={styles.companylogo} width={20} height={20}/>
+        <h2 className={styles.logo}>Aid <span className={styles.txt}>Tracker</span></h2>
+        </div>
       <nav className={styles.nav}>
         <a href="#home">Home</a>
         <a href="#how-it-works">How It Works</a>
